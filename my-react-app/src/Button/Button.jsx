4ce8779a9d39daae086cbf1567aeabc8c5cbd981
Hithesh C H <hitheshch2000@gmail.com>
2024-10-09 19:24:18 +0200

    EventClick

--- my-react-app/src/Button/Button.jsx
+++ my-react-app/src/Button/Button.jsx
@@ -8,7 +8,21 @@ import PropTypes from 'prop-types'
 
 function Button()
 {
-return <button className={styles.button}> Click Me</button>
+let count=0;
+
+const handleClick= (name) =>
+   {
+    if(count <3)
+    {
+        count++;
+        console.log(` ${name} you clicked me ${count} time`);
+    }
+    else{
+        console.log(`${name} stop`)
+    }
+   }
+
+return <button className={styles.button} onClick={()=>handleClick("Hithesh")}> Click Me</button>
 }
 
 Student.PropTypes={
