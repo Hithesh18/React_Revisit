405d874fd6372b437873a9475f389d38f5a89fa5
Hithesh C H <hitheshch2000@gmail.com>
2024-10-09 11:34:16 +0200

    CardComponent

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,13 @@
+import profilePic from './assets/Profile.jpeg'
+function Card()
+{
+return(
+    <div className="card">
+        <img className="card-image"src="./src/assets/Profile.jpeg" alt="Profile Picture" />
+        <h2 className="card-title">Hithesh18</h2>
+        <p className="card-text">Masters in Computer Science</p>
+
+    </div>
+)
+}
+export default Card
